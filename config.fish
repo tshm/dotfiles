822e4c61193eaa2dcb9597840c2537fa736b9132
Tosh Shimayama <2sm@csc.jp>
2014-03-04 16:11:41 +0900

    fish: local config inclusion

--- config.fish
+++ config.fish
@@ -2,16 +2,18 @@ set -x SHELL /usr/bin/fish
 
 if status --is-login
   cd $HOME
-  test -f .local.fish; and source .local.fish
+  test -f .login.fish; and source .login.fish
 end
 
 if status --is-interactive
+  test -f .local.fish; and source .local.fish
   set -x LESS "--quit-if-one-screen -R -iMX"
   set -x LS_COLORS "di=32:ln=36:pi=31:so=33:bd=44;37:cd=44;37:ex=35"
   set -x PATH ~/bin $PATH
 
   alias dir "ls -lh"
   alias rm "rm -i"
+  alias vim "env SHELL=sh vim"
 
   function fish_user_key_bindings
     bind \ek history-token-search-backward
