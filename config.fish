2c99111312cbce121dbfe216caf6f282f934e2c8
Tosh Shimayama <2sm@csc.jp>
2014-03-05 10:11:13 +0900

    add zsh keybindings

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ set -x SHELL /usr/bin/fish
 
 if status --is-login
   cd $HOME
+  # which tmux; and tmux attach -t root; or tmux new -s root
   test -f .login.fish; and source .login.fish
 end
 
@@ -15,14 +16,32 @@ if status --is-interactive
   alias rm "rm -i"
   alias vim "env SHELL=sh vim"
 
+  set __QQ
+  function queuecommand
+    if test -z "$argv" -a -n "$__QQ"
+      set -l n (count $__QQ)
+      commandline -i $__QQ[$n]
+      set -e __QQ[$n]
+    else
+      set __QQ $__QQ "$argv"
+    end
+  end
+
   function fish_user_key_bindings
-    bind \ek history-token-search-backward
-    bind \ej history-token-search-forward
+    bind \ek  history-token-search-backward
+    bind \ej  history-token-search-forward
+    # commandline seems to eval from right to left...
+    bind \e\; 'commandline -f execute accept-autosuggestion'
+    # invoking manual
+    bind \eh 'man (commandline -po)[1]; commandline -f repaint'
+    # command queueing
+    bind \eq 'queuecommand (commandline); commandline -f backward-kill-line'
   end
 
   function fish_right_prompt
     set_color yellow
     printf '(%s) ' (date +%H:%M)
+    queuecommand
     set_color normal
   end
 end
