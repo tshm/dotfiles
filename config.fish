353a7f0bd477486f1322c465c27c7643b21d7602
Tosh Satake <2sm@csc.jp>
2015-12-21 12:55:24 +0900

    integrate with fzf

--- config.fish
+++ config.fish
@@ -16,39 +16,92 @@ if status --is-interactive
   alias rm "rm -i"
   alias vim "env SHELL=sh vim"
 
+  # helper functions
   set __CMD_STACK
-  function push_cmd
+  function _push_cmd
     set __CMD_STACK $__CMD_STACK "$argv"
   end
 
-  function pop_cmd
-    test -z "$__CMD_STACK"; and return
+  function _pop_cmd
+    [ -z "$__CMD_STACK" ]; and return
     set -l n (count $__CMD_STACK)
     commandline -i $__CMD_STACK[$n]
     set -e __CMD_STACK[$n]
   end
 
+  function _custom_execute
+    execute
+  end
+
+  function _quote
+    set -l tok (commandline -t)
+    commandline -t \"$tok\"
+    [ -z "$tok" ]; and commandline -f backward-char
+  end
+
+  function _stack
+    _push_cmd (commandline)
+    commandline -f kill-whole-line
+  end
+
+  function _help
+    echo -- get help
+    man (commandline -po)[1]
+    commandline -f repaint
+  end
+
+  function _fzf_pane
+    [ -z "$TMPDIR" ]; and set -g TMPDIR /tmp
+    tmux capture-pane -pS -30 | eval "fzf-tmux -d40%" > $TMPDIR/fzf.result
+    set -l __FZF (cat $TMPDIR/fzf.result)
+    cat $TMPDIR/fzf.result
+    [ -z "$__FZF" ]; and return
+    commandline -i $__FZF
+    commandline -o
+    commandline -f repaint
+  end
+
+  function _fzf_file
+    [ -z "$TMPDIR" ]; and set -g TMPDIR /tmp
+    ls | eval "fzf-tmux -d40%" > $TMPDIR/fzf.result
+    set -l __FZF (cat $TMPDIR/fzf.result)
+    cat $TMPDIR/fzf.result
+    [ -z "$__FZF" ]; and return
+    commandline -i $__FZF
+    commandline -o
+    commandline -f repaint
+  end
+
   function fish_user_key_bindings
-    bind \ek  history-token-search-backward
-    bind \ej  history-token-search-forward
+    # enable vi mode
+    fish_vi_mode
+    fish_vi_key_bindings
+    # delete exit shortcut
+    bind -e \cd
+    bind -e \ed
+    # find filename via fzf
+    bind -M insert \e8  _fzf_file
+    bind -M insert \e9  _fzf_pane
+    bind -M insert \ek  history-token-search-backward
+    bind -M insert \ej  history-token-search-forward
     # accept and run
-    bind \e\; 'commandline -f accept-autosuggestion execute'
+    bind -M insert \e\; 'commandline -f accept-autosuggestion execute'
     # invoking manual
-    bind \eh 'echo get help;man (commandline -po)[1]; commandline -f repaint'
+    bind -M insert \eh _help
     # command stack
-    bind \eq 'push_cmd (commandline); commandline -f kill-whole-line'
+    bind -M insert \eq _stack
     # backward-kill-word
-    bind \e\b backward-kill-word
+    bind -M insert \e\b backward-kill-word
     # quote token
-    bind \e7 'set -l tok (commandline -t); commandline -t \"$tok\"; [ -z $tok ] ;and commandline -f backward-char'
+    bind -M insert \e7 _quote
   end
 
   function fish_right_prompt
-    set_color yellow
+    set_color -u yellow
     printf '(%s) ' (date +%H:%M)
-    pop_cmd
+    _pop_cmd
     set_color normal
   end
 end
 
-# vi: ft=vb
+# vi: ft=zsh
