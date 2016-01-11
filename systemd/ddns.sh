194d0e885cb7d5d5d1d898dc6778c32eb8b5d682
tshm <2sm@csc.jp>
2016-01-11 21:31:50 +0900

    only request when ip updates

--- systemd/ddns.sh
+++ systemd/ddns.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+cd $(dirname $0)
 
 HOST=insp.hn.org
-PASS=`cat ./.ddns.passwd`
+PASS=$(cat ./.ddns.passwd)
 URL=https://inspzqj32v3.hn.org/update.php 
-IP=`curl -s http://ipinfo.io/ip`
+RIP=$(host $HOST | cut -d' ' -f4)
+IP=$(curl -s http://ipinfo.io/ip)
 
+echo RIP=$RIP
 echo IP=$IP
+[ "$RIP" = "$IP" ] && exit
 
 curl -s --data-urlencode hostname=$HOST \
         --data-urlencode password=$PASS \
