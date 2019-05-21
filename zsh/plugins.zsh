8c7b45a51fb9e79c6faa76a6385dfacc809bc84a
Tosh Satake <2sm@csc.jp>
2019-05-21 13:41:47 +0900

    add magick quote

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,25 +1,23 @@
-echo "handle plugins"
+# echo "handle plugins"
 
 # init
 source ~/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-# plugin at load time
 zplugin light zdharma/fast-syntax-highlighting
+#zplugin ice wait'1' atload'_zsh_autosuggest_start'
 zplugin light zsh-users/zsh-autosuggestions
 zplugin light zsh-users/zsh-completions
 
-zplugin ice as"program" pick"bin/git-dsf"
-zplugin light zdharma/zsh-diff-so-fancy
+zplugin ice as"program" pick"bin/git-dsf"; zplugin load zdharma/zsh-diff-so-fancy
 zplugin light nocttuam/autodotenv
 
-# async loading
-zplugin ice pick"async.zsh" src"pure.zsh"; zplugin light sindresorhus/pure
-zplugin ice wait'!0'; zplugin light zsh-users/zsh-history-substring-search
-zplugin ice wait'!0'; zplugin light vim/vim
+zplugin ice pick"async.zsh" src"pure.zsh"; zplugin load sindresorhus/pure
+zplugin light zsh-users/zsh-history-substring-search
+zplugin light vim/vim
 zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
-zplugin ice "rupa/z" pick"z.sh"; zplugin light "rupa/z"
+zplugin ice "rupa/z" pick"z.sh"; zplugin load "rupa/z"
 #zplugin load zpm-zsh/clipboard
 zplugin light zsh-users/zaw
 
@@ -29,9 +27,4 @@ zplugin snippet 'OMZ::lib/compfix.zsh'
 # finalize
 autoload -U compinit
 compinit
-
-# Also prezto
-#zplug "modules/prompt", from:prezto
-
-#zplug "chrissicool/zsh-256color"
 
