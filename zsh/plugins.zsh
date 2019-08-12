8d651d49eb649676570321a5c333024206f957de
Tosh Satake <2sm@csc.jp>
2019-08-12 13:19:24 +0900

    cleanup

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,26 +5,27 @@ source ~/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-#zplugin light zdharma/fast-syntax-highlighting
-#zplugin ice wait'1' atload'_zsh_autosuggest_start'
-zplugin light zsh-users/zsh-autosuggestions
-zplugin light zsh-users/zsh-completions
-zplugin light mollifier/anyframe
+#zplg light zdharma/fast-syntax-highlighting
+#zplg ice wait'1' atload'_zsh_autosuggest_start'
+zplg light zsh-users/zsh-autosuggestions
+zplg light zsh-users/zsh-completions
+zplg light mollifier/anyframe
 
-zplugin ice as"program" pick"bin/git-dsf"; zplugin load zdharma/zsh-diff-so-fancy
-zplugin light nocttuam/autodotenv
+zplg ice wait"0" as"program" pick"bin/git-dsf"; zplg load zdharma/zsh-diff-so-fancy
+zplg ice wait"0"; zplg light nocttuam/autodotenv
 
-zplugin ice pick"async.zsh" src"pure.zsh"; zplugin load sindresorhus/pure
-zplugin light zsh-users/zsh-history-substring-search
+zplg ice pick"async.zsh" src"pure.zsh"; zplg load sindresorhus/pure
+zplg ice wait"0"; zplg light zsh-users/zsh-history-substring-search
+
 if [ "$LOAD_FZF" -ne "0" ]; then
-    zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
+    zplg ice from"gh-r" as"program"; zplg load junegunn/fzf-bin
 fi
-zplugin ice "rupa/z" pick"z.sh"; zplugin load "rupa/z"
-#zplugin load zpm-zsh/clipboard
-zplugin light zsh-users/zaw
+zplg ice "rupa/z" pick"z.sh"; zplg load "rupa/z"
+#zplg load zpm-zsh/clipboard
+#zplg light zsh-users/zaw
 
-zplugin snippet 'OMZ::lib/completion.zsh'
-zplugin snippet 'OMZ::lib/compfix.zsh'
+zplg snippet 'OMZ::lib/completion.zsh'
+zplg snippet 'OMZ::lib/compfix.zsh'
 
 # finalize
 autoload -U compinit
