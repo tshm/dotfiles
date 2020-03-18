318c7c5c8f8e10147521d774538357945ec2248d
Tosh Satake <satake@empathy.co.jp>
2020-03-18 22:28:07 +0900

    update plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -15,25 +15,33 @@ zinit ice wait'1' atload'_zsh_autosuggest_start'
 zinit light zsh-users/zsh-completions
 zinit ice as"program" pick"bin/git-dsf"; zinit load zdharma/zsh-diff-so-fancy
 
-zinit ice wait
 zinit load lukechilds/zsh-nvm
 
-zinit ice as"command" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit ice wait:2 as"command" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
+zinit ice wait:2 as"command" from"gh-r" mv"fd* -> fd" pick"fd/fd"
+zinit light sharkdp/fd
 
-# zinit ice wait:2 lucid extract"" from"gh-r" as"program" mv"exa* -> exa"
-# zinit light ogham/exa
-# zinit light DarrinTisdale/zsh-aliases-exa
+zinit ice wait:2 lucid extract"" from"gh-r" as"command" mv"exa* -> exa"
+zinit light ogham/exa
+zinit light DarrinTisdale/zsh-aliases-exa
 
-zinit ice from"gh-r" as"program" mv"direnv* -> direnv"
+zinit ice wait:2 as"command" from"gh-r" mv"ripgrep* -> ripgrep" pick"ripgrep/rg"
+zinit light BurntSushi/ripgrep
+
+zinit ice wait:2 from"gh-r" as"command" mv"direnv* -> direnv"
 zinit light direnv/direnv
+
+zinit ice wait:2 lucid from"gh-r" pick bpick"*.gz"
+zinit light starship/starship
 
 zinit ice as"completion"
 zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
 
 zinit light zsh-users/zsh-history-substring-search
 if [ -z "$LOAD_FZF" -o "$LOAD_FZF" -ne "0" ]; then
-    zinit ice from"gh-r" as"program"; zinit load junegunn/fzf-bin
+    zinit ice from"gh-r" as"command"
+    zinit load junegunn/fzf-bin
 fi
 zinit ice "rupa/z" pick"z.sh"; zinit load "rupa/z"
 zinit ice pick"deer"; zinit load "Vifon/deer"
