2c45f16846555702addf8883b0d7bff3accf787c
Tosh Satake <satake@empathy.co.jp>
2022-06-27 11:40:50 +0900

    fix(nix): use home-manager 20.05

--- home.nix
+++ home.nix
@@ -2,6 +2,9 @@
 {
   # imports = [ ./base.nix ];
   programs.home-manager.enable = true;
+  home.stateVersion = "22.05";
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
   home.packages = with pkgs; [
     tig
     tmux
