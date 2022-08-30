ef15e201b6a7e042f7b113ac71160432a32e0617
Tosh Satake <satake@empathy.co.jp>
2022-08-30 10:14:03 +0900

    fix: update sample.sh

--- proj/sample.sh
+++ proj/sample.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cd ~
+DIR=~
 # 0:root
-tmux new-session -s $SN -d
+tmux new-session -s $SN -c $DIR -d
 # 1:dotfiles
-tmux neww -t $SN:1 -c .dotfiles
+tmux neww -t $SN:1 -c $DIR/.dotfiles
 tmux send -t $SN:1 'git status' Enter
 # 2:ssh
-tmux neww -t $SN:2 'autossh -M 20000 spi';
+tmux neww -t $SN:2 -c $DIR 'autossh -M 20000 spi'
 # tmux splitw -t $SN:2 -v -l 50% 'autossh test';
 # tmux select-pane -t 0
 
