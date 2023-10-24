0af4c29702110b3eccb7b088ad966666fcb3ecfc
Tosh Satake <satake@empathy.co.jp>
2023-10-24 18:46:56 +0900

    setup codeuim via LazyExtras

--- vim/nvim/lua/plugins/core.lua
+++ vim/nvim/lua/plugins/core.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "jcdickinson/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = { "mvllow/modes.nvim" },
     ---@param opts cmp.ConfigSchema
