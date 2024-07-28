6b1ef16249230196faa0bfb0323a76a0d3e9ace0
Tosh Satake <2sm@csc.jp>
2024-07-29 07:43:12 +0900

    fix(wezterm): add mouse binding

--- wezterm/wezterm_base.lua
+++ wezterm/wezterm_base.lua
@@ -11,8 +11,35 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.color_scheme = "Earthsong"
 config.color_scheme = "Bamboo"
 
+config.audible_bell = "Disabled"
+config.adjust_window_size_when_changing_font_size = false
+
 -- config.font = wezterm.font("FiraMono Nerd Font")
 -- config.default_prog = { "/usr/bin/zsh", "-l" }
 -- config.default_prog = { "wsl" }
 -- config.default_domain = "WSL:Ubuntu"
+
+config.mouse_bindings = {
+	{
+		event = {
+			Up = {
+				streak = 1,
+				button = "Left",
+			},
+		},
+		mods = "NONE",
+		action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("Clipboard"),
+	},
+	{
+		event = {
+			Down = {
+				streak = 1,
+				button = "Right",
+			},
+		},
+		mods = "NONE",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+}
+
 return config
