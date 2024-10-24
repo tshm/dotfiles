ae2c8a64f687f26b28343b9629bc041f93e9e5ab
Tosh Satake <2sm@csc.jp>
2024-10-25 08:03:09 +0900

    fix(hypr): adjust icons

--- wlscan.sh
+++ wlscan.sh
@@ -7,12 +7,12 @@ logger "bring up $IF"
 /sbin/ifconfig "$IF" up
 A='No scan results'
 while [ "$A" = "No scan results" ]; do
-	sleep 1
-	A=$(iwlist scan 2>/dev/null)
+  sleep 1
+  A=$(iwlist scan 2>/dev/null)
 done
 /sbin/ifconfig "$IF" down
 
 while read -r key name; do
-	echo "$A" | grep -q "$key" && logger "ESSID: $name" && echo "$name" && exit 0
+  echo "$A" | grep -q "$key" && logger "ESSID: $name" && echo "$name" && exit 0
 done
 exit 1
