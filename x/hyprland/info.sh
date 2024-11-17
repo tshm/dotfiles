59b51a77efe85e782e76f90397dbda11e3767674
Tosh Satake <2sm@csc.jp>
2024-11-17 20:20:14 +0900

    fix: update info.sh

--- x/hyprland/info.sh
+++ x/hyprland/info.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+[ -d /sys/class/power_supply/BAT0 ] &&\
+  BAT="Battery $(cat /sys/class/power_supply/BAT0/status) : $(cat /sys/class/power_supply/BAT0/capacity)%"
+
+BT=$(bluetoothctl devices Connected | cut -d' ' -f3)
+[ -n "$BT" ] && BT="Bluetooth: $BT\n"
+
+VPN=$(warp-cli status | cut -d' ' -f3)
+[ -n "$VPN" ] && VPN="VPN: $VPN\n"
+
 notify-send "$(date '+%Y/%m/%d %H:%M')" \
-  "Bluetooth: $(bluetoothctl devices Connected | cut -d' ' -f3)\n\
-  VPN: $(warp-cli status | cut -d' ' -f3)"
+  "$BT$VPN$BAT"
 
