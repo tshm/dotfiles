84d85782c2321d7d9dedf96d4f171576bbbd48b5
Tosh Satake <2sm@csc.jp>
2024-12-04 18:23:20 +0900

    feat(util): add utility rofi script

---
+++ x/hyprland/util.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+options=()
+
+warp-cli status | grep Connected &&
+  warpcmd="✅ Warp Disconnect: warp-cli disconnect; warp-cli status" ||
+  warpcmd="❌ Warp Connect: warp-cli connect; warp-cli status"
+options+=("$warpcmd")
+
+btdevice=$(bluetoothctl devices Connected)
+[ -n "$btdevice" ] && {
+  addr=$(echo $btdevice | cut -d' ' -f2)
+  name=$(echo $btdevice | cut -d' ' -f3-)
+  btcmd="📶 Reconnect BluetoothDevice $name: bluetoothctl disconnect $addr; bluetoothctl connect $addr"
+  options+=("$btcmd")
+}
+echo $btcmd
+
+# Define the menu options
+options+=(
+  "💤 Suspend: systemctl suspend"
+  "⏹️ Shutdown: systemctl poweroff"
+)
+
+# Display the options using Rofi
+selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "Select an action:")
+
+# Execute the selected command
+if [[ -n "$selected" ]]; then
+  cmd=$(echo $selected | cut -d: -f2- | xargs)
+  result=$(eval $cmd)
+  [ "$?" = "0" ] && icon=✅ || icon=❌ 
+  notify-send "$icon" "$result"
+fi
+
