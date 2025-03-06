2b98f4d198384ee27ecc74b9447b0d70f54a21eb
Tosh Satake <2sm@csc.jp>
2025-03-06 12:51:04 +0900

    fix(gui): use wezterm

--- wezterm/wezterm_base.lua
+++ wezterm/wezterm_base.lua
@@ -14,7 +14,15 @@ config.color_scheme = "Bamboo"
 config.audible_bell = "Disabled"
 config.adjust_window_size_when_changing_font_size = false
 
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font_with_fallback({
+	{ family = "FiraCode Nerd Font" },
+	{ family = "Noto Sans CJK JP" },
+	-- { family = "Noto Sans CJK JP", assume_emoji_presentation = true },
+	-- { family = "Noto Sans Mono", assume_emoji_presentation = true },
+	-- { family = "Source Han Code JP L", assume_emoji_presentation = false },
+	-- { family = "Source Han Code JP L", assume_emoji_presentation = true },
+})
+config.font_size = 11
 -- config.default_prog = { "/usr/bin/zsh", "-l" }
 -- config.default_prog = { "wsl" }
 -- config.default_domain = "WSL:Ubuntu"
