504d49a414684fa3f934ca1fd779904655923b44
tshm <6375+tshm@users.noreply.github.com>
2025-03-26 22:55:54 +0000

    Update apps

--- homes/apps/zen.nix
+++ homes/apps/zen.nix
@@ -5,7 +5,7 @@ let
   src = pkgs.fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-1NoDIdzeADVLOhtesR0QZefhAdRBSNWg4++MjqdkfIs=";
+    sha256 = "sha256-ZqxXM2DhnAZBZQBBPyIa5CO94DiO4cFB07EqtmGEdIM=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in
