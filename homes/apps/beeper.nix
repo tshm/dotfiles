a1f30bb10351f097cb4f1474f8d50774057dd578
Tosh Satake <2sm@csc.jp>
2025-05-24 08:14:56 +0900

    chore: update

--- homes/apps/beeper.nix
+++ homes/apps/beeper.nix
@@ -6,7 +6,7 @@ let
     curlOptsList = [ "-L" "-H" "Accept:application/octet-stream" ];
     url = "https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-+m4pmDYnn4fWAp8Ve7hjNaFClPU8tLIAVGQSJNlo/p0=";
+    sha256 = "sha256-8NwntTrxxWScww0i2DlbVUg5KBlE3WcCVq5P1so8/ig=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in
--- homes/apps/zen.nix
+++ homes/apps/zen.nix
@@ -5,7 +5,7 @@ let
   src = pkgs.fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-HnEXMPvRwOacsMJ/qDOKIx9zYgQ+PnX4KLMcqcty5sE=";
+    sha256 = "sha256-974n8beJnJTCgC7a/jwyA7MDuhkY2FidX0iimTqIVIg=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in
