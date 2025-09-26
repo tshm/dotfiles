a0047ecc97eef87949e1afd8359ae39ff5213550
Tosh Satake <2sm@csc.jp>
2025-09-27 02:42:27 +0900

    chore: update zen

--- homes/apps/zen.nix
+++ homes/apps/zen.nix
@@ -5,7 +5,7 @@ let
   src = pkgs.fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-tHQhMxFLbkMZVHfbNBgg5ENqDHOC6ZIcSru4r132ZIg=";
+    sha256 = "sha256-nXbaopCq037Syt2XWPu382cWczysGNLkwEb5RORmga0=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in
