bbf4d84dcfa9d98c185eb17f00125208df49aa40
Tosh Satake <2sm@csc.jp>
2025-09-28 07:47:25 +0900

    chore: add pub key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,6 @@
 let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1nFDHaQvTrUEDiPpT3qvVJXEot5IEhBJmUZ0WKRPYD";
+  key1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1nFDHaQvTrUEDiPpT3qvVJXEot5IEhBJmUZ0WKRPYD";
+  key2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOe16XtRvMF6S+1Z0tkk3R7jV211Ff2ynmoL+BinKmwW";
 in {
-  "user-password.age".publicKeys = [ key ];
+  "user-password.age".publicKeys = [ key1 key2 ];
 }
