fc45ddeed74336ed788854aed6ef18b909a17edf
Tosh Satake <2sm@csc.jp>
2025-12-16 08:32:51 +0900

    adjust tools

--- x/night.sh
+++ x/night.sh
@@ -3,4 +3,4 @@ CONTENT=$(curl -s http://ip-api.com/json/)
 echo $CONTENT
 longitude=$(echo $CONTENT | jq .lon)
 latitude=$(echo $CONTENT | jq .lat)
-wlsunset -l $latitude -L $longitude 
+wlsunset -l $latitude -L $longitude
