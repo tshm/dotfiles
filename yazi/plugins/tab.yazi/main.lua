c719c485526f888e43a07843944bbf2491e359fc
Tosh Satake <2sm@csc.jp>
2026-01-21 09:59:09 +0900

    fix(yazi): adapt API change

--- yazi/plugins/tab.yazi/main.lua
+++ yazi/plugins/tab.yazi/main.lua
@@ -2,9 +2,9 @@
 return {
 	entry = function(self, args)
 		if #cx.tabs > 1 then
-			ya.manager_emit("tab_switch", { 1, relative = true })
+			ya.emit("tab_switch", { 1, relative = true })
 		else
-			ya.manager_emit("tab_create", { current = true })
+			ya.emit("tab_create", { current = true })
 		end
 	end,
 }
