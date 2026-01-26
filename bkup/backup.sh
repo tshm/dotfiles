2b435afefb961b53c31445150548ae1ee841ecfa
Tosh Satake <2sm@csc.jp>
2026-01-26 15:57:27 +0900

    fix: backup only changes

--- bkup/backup.sh
+++ bkup/backup.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+cd $(dirname "$0") || exit
 eval "$(sed '/^#/d; /^$/d; s/^/export /' .*env | xargs -0)"
-restic backup "$BKUP_SRC"
+restic backup --skip-if-unchanged "$BKUP_SRC"
 restic forget --keep-weekly 5 --keep-monthly 5 --keep-yearly 2 --prune
