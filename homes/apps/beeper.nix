b7efb93a23e67f7bca3c6de8be39ef3284be14a8
Tosh Satake <2sm@csc.jp>
2026-03-29 15:23:14 +0900

    chore: update

--- homes/apps/beeper.nix
+++ homes/apps/beeper.nix
@@ -6,7 +6,7 @@ let
     curlOptsList = [ "-L" "-H" "Accept:application/octet-stream" ];
     url = "https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-AvcERjQizf/8MsFjC9NtkMne/2ZFLPxiL0HMQsyo8TE=";
+    sha256 = "sha256-0oZNAweL4hJt5RQCMYHqfqvv/+9dWv0SsyvYRlpUqTc=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in pkgs.appimageTools.wrapType2 {
--- homes/apps/zen.nix
+++ homes/apps/zen.nix
@@ -5,7 +5,7 @@ let
   src = pkgs.fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-apJ/3U1XSSyTEQGFVVXMSd/zJYTzpvsn1x6bQPC8b7A=";
+    sha256 = "sha256-p00Irv2z6brDXMx3cr0234lOZZ2a7FmJMDzN494nzMw=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in pkgs.appimageTools.wrapType2 {
