56ab2c051d12cb3045c8e13138210b5ac6a3a1af
Tosh Satake <2sm@csc.jp>
2026-04-06 09:32:05 +0900

    fix: wezterm bell

--- wezterm/wezterm_base.lua
+++ wezterm/wezterm_base.lua
@@ -11,7 +11,7 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.color_scheme = "Earthsong"
 config.color_scheme = "Bamboo"
 
-config.audible_bell = "Enabled"
+config.audible_bell = "SystemBeep"
 config.adjust_window_size_when_changing_font_size = false
 
 config.font = wezterm.font_with_fallback({
