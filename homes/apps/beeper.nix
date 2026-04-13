2060c9a63d4dce0f10a65956a1e544a08a80753d
Tosh Satake <2sm@csc.jp>
2026-04-14 08:15:35 +0900

    fix: update

--- homes/apps/beeper.nix
+++ homes/apps/beeper.nix
@@ -6,7 +6,7 @@ let
     curlOptsList = [ "-L" "-H" "Accept:application/octet-stream" ];
     url = "https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop";
     # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-h83tTulmb3HR1NClXyM2xp/5303adMwbouCQHiOG3MM=";
+    sha256 = "sha256-dz3Hg6HmV1T2NrzG+bbIbCPUweZThAnTGVQ4rYpyz0k=";
   };
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in pkgs.appimageTools.wrapType2 {
