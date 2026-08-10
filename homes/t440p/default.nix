1ec32f5d110a3a7b0276d61f5d55756ebc162535
Tosh Satake <2sm@csc.jp>
2026-08-10 09:14:52 +0900

    fix: make tp a mutica runtime

--- homes/t440p/default.nix
+++ homes/t440p/default.nix
@@ -14,6 +14,19 @@
       ../modules/dev.nix
       ../modules/playwright.nix
       {
+        systemd.user.services.multica = {
+          Unit = {
+            Description = "Multica selfhost daemon";
+            StartLimitBurst = 10;
+            StartLimitIntervalSec = "infinity";
+          };
+          Service = {
+            ExecStart = "/usr/local/bin/multica daemon start --profile selfhost";
+            Restart = "on-failure";
+          };
+          Install.WantedBy = [ "default.target" ];
+        };
+
         home.packages = [
           pkgs.qsv
           pkgs.deno
